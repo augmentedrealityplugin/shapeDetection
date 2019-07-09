1e1eae219e9c2bd118e1d296707fe06583d94a51
Kushagra Bhargava <emailkushagra@gmail.com>
2019-07-09 01:52:41 -0700

    [FEAT] Plugin Release v1.0

---
+++ Plugin/ShapeDetection.framework/Headers/ShapeDetection.h
@@ -0,0 +1,19 @@
+//
+//  ShapeDetection.h
+//  ShapeDetection
+//
+//  Created by Behram  Buhariwala on 7/8/19.
+//  Copyright © 2019 Behram  Buhariwala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShapeDetection.
+FOUNDATION_EXPORT double ShapeDetectionVersionNumber;
+
+//! Project version string for ShapeDetection.
+FOUNDATION_EXPORT const unsigned char ShapeDetectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShapeDetection/PublicHeader.h>
+
+
